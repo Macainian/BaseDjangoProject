a38618db97113f92173b44cb08ed2a019dd908cb
Keith Hostetler <macainian@gmail.com>
2017-01-15 05:18:01 -0500

    Fixed urls for account_manager.

--- website/apps/account_manager/urls.py
+++ website/apps/account_manager/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import url
 
 from website.apps.account_manager.views.class_based.BrowseView import BrowseView
 from website.apps.account_manager.views.class_based.DeleteView import DeleteView
+from website.apps.account_manager.views.class_based.DetailsView import DetailsView
 from website.apps.account_manager.views.class_based.EditView import EditView
 
 urlpatterns = [
     url(r"^browse/$", BrowseView.as_view(), name="account_manager.browse"),
     url(r"^edit/(?P<account_id>.*)/$", EditView.as_view(), name="account_manager.edit"),
     url(r"^delete/$", DeleteView.as_view(), name="account_manager.delete"),
-
-    # url(r"^example_class_view/$", ExampleClassView.as_view(), name="account_manager.example_class_view"),
-    # url(r"^example_function_view/$", example_function_view, name="account_manager.example_function_view"),
-    # url(r"^example_parameter_passing/(?P<example_var>\w+)/$", example_view, name="account_manager.example_parameter_passing"),
+    url(r"^details/(?P<account_id>.*)/$", DetailsView.as_view(), name="account_manager.details"),
 ]
